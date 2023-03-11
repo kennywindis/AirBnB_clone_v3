c276339677a99a1f153cc02c94ead39143a0a44c
kennywindis <kennyphilip100@gmail.com>
2023-03-11 04:24:03 -0800

    ab3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" Init file """
+"""Creating a flask app Blueprints
+"""
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_views', 'app_views', url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
