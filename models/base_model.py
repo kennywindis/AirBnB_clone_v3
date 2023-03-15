a261a5b56e3317c08c6fab649305444dc5894473
kennywindis <kennyphilip100@gmail.com>
2023-03-15 00:42:52 -0700

    ab3

--- models/base_model.py
+++ models/base_model.py
@@ -36,16 +36,19 @@ class BaseModel:
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
-                self.created_at = datetime.now()
+                self.created_at = datetime.utcnow()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
-                self.updated_at = datetime.now()
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("password") is not None:
+                self.password = (hashlib.md5(
+                                 kwargs['password'].encode()).hexdigest())
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+            self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
 
     def __str__(self):
@@ -59,19 +62,18 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, dump=None):
+    def to_dict(self, show_pswd=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        if new_dict.get('password') is not None and show_pswd is False:
+            del new_dict["password"]
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if getenv("HBNB_TYPE_STORAGE") == "db":
-            if 'password' in new_dict:
-                del new_dict["password"]
         return new_dict
 
     def delete(self):
